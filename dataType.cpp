2a4821ded5c4312cf255dce82ce905b855a59bc1
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-10-31 16:25:46 +0530

    input

---
+++ dataType.cpp
@@ -0,0 +1,12 @@
+// Intiger Data type take 4 bytes... 8bit--1byte 
+// Charater 0data otype take 1 byte....char datatype name 
+//boolean data type take 1 byte  ...bool flg= true / false
+//Floating Data type allow decimal / exponential...float no= 1. ..7decimal....8byte
+// Double Floating .....upto 15 decimal
+
+// void... return nothing
+
+//wide charecter... 
+
+// string data type ....sequence 0data type  store    like ""
+//
