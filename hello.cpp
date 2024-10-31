59522238863c3560c4482a437084a3305651e6f5
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-10-31 16:14:42 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>/// header file include in program
+
+using namespace std; // use like std : cout ....add in every line 
+
+int main(){   // code execute start always in main.........
+             //......return type ...function name  (parameter)
+
+    cout<<"hello"<< endl<< "world";  // <<endl << for next line  or '\'
+    
+    return 0; // end the program function
+}
