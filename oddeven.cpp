d73f891d9e88e7f42c9ef22780305715fa07c770
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-10-31 21:07:20 +0530

    oddeven

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int no ;
+
+    cin>>no;
+
+    if(no%2==0){
+        cout << no << "is even";
+    }else{
+        cout << no << "is odd";
+    }
+}
